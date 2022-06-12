e6334522d95e21262bcab77a686c296d51139a6a
Permitted <permittedbr@gmail.com>
2022-06-12 23:39:32 +0300

    docker fix and database connection added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/Permitted/go-rest-api/internal/database"
+)
 
 // Run - is going to responsible for
 // the instantiation and start up of
 // go application
 func Run() error {
 	fmt.Println("Starting application.")
+
+	db, err := database.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
 	return nil
 }
 
